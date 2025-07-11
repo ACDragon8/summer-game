bb869933745da64f4e6b9216523052186795bcd7
CynthiaHe0 <67443885+CynthiaHe0@users.noreply.github.com>
2025-07-11 13:40:10 -0700

    Made it so the button only triggers if you click it
    Also made the turn swap wait for enemy animation to finish

--- Scenes/enemy.gd
+++ Scenes/enemy.gd
@@ -2,17 +2,22 @@ extends AnimatedSprite2D
 
 signal enemy_attack()
 
+
+func _ready() -> void:
+	pass
+
 func attack() -> void:
-	var tween =  get_tree().create_tween()
-	tween.tween_property(self,"global_position",position + Vector2(-10, 10),0.2)
-	tween.tween_property(self,"global_position",position + Vector2(10, -10),0.2)
+	var atk = get_tree().create_tween()
+	var old_position = position
+	atk.tween_property(self,"global_position", position + Vector2(-10, 10), 0.2)
+	atk.tween_property(self,"global_position", old_position, 0.2)
+	atk.connect("finished", on_tween_finished)
+	enemy_attack.emit()
 	
 
 
 func _on_end_turn_button_end_player_turn() -> void:
 	attack()
-	enemy_attack.emit()
-	#SPAGHETTI ALERT
-	# I probably should create a global turn manager for this
-	# for now, we are gonna have the enemy end enemy turn
+
+func on_tween_finished() -> void:
 	DragAndDrop.player_turn = true
